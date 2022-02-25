97c6c6a4f5cc6554be3cb818ef8dc73df528e490
Yuekai Jia <equation618@gmail.com>
2022-02-25 20:23:45 +0800

    ch2: check and switch to EL1 on entry

--- os/src/entry.rs
+++ os/src/entry.rs
@@ -1,17 +1,58 @@
 use core::arch::asm;
 
+use cortex_a::{asm, registers::*};
+use tock_registers::interfaces::{ReadWriteable, Readable, Writeable};
+
 const BOOT_STACK_SIZE: usize = 4096 * 16;
 
 #[link_section = ".bss.stack"]
 static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
+
+unsafe fn switch_to_el1() {
+    let current_el = CurrentEL.read(CurrentEL::EL);
+    if current_el >= 2 {
+        if current_el == 3 {
+            // Set EL2 to 64bit and enable the HVC instruction.
+            SCR_EL3.write(
+                SCR_EL3::NS::NonSecure + SCR_EL3::HCE::HvcEnabled + SCR_EL3::RW::NextELIsAarch64,
+            );
+            // Set the return address and exception level.
+            SPSR_EL3.write(
+                SPSR_EL3::M::EL1h
+                    + SPSR_EL3::D::Masked
+                    + SPSR_EL3::A::Masked
+                    + SPSR_EL3::I::Masked
+                    + SPSR_EL3::F::Masked,
+            );
+            ELR_EL3.set(LR.get());
+        }
+        // Disable EL1 timer traps and the timer offset.
+        CNTHCTL_EL2.modify(CNTHCTL_EL2::EL1PCEN::SET + CNTHCTL_EL2::EL1PCTEN::SET);
+        CNTVOFF_EL2.set(0);
+        // Set EL1 to 64bit.
+        HCR_EL2.write(HCR_EL2::RW::EL1IsAarch64);
+        // Set the return address and exception level.
+        SPSR_EL2.write(
+            SPSR_EL2::M::EL1h
+                + SPSR_EL2::D::Masked
+                + SPSR_EL2::A::Masked
+                + SPSR_EL2::I::Masked
+                + SPSR_EL2::F::Masked,
+        );
+        ELR_EL2.set(LR.get());
+        asm::eret();
+    }
+}
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 unsafe extern "C" fn _start() -> ! {
     asm!("
         mov     sp, {boot_stack_top}
+        bl      {switch_to_el1}
         b       {rust_main}",
         boot_stack_top = in(reg) BOOT_STACK.as_ptr_range().end,
+        switch_to_el1 = sym switch_to_el1,
         rust_main = sym crate::rust_main,
         options(noreturn),
     )
